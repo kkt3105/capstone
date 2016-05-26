6a3390881b77eaa366785600d7321e06ec3495ca
kyeongtae <ktk3105@gmail.com>
2016-05-26 19:16:06 +0900

    to fix bugs

--- controllers/info-controller.js
+++ controllers/info-controller.js
@@ -71,6 +71,10 @@ exports.setHR = function(req, res){
 exports.senior = function(req, res){
         var db_flag = false;
         var jsonData = {};
+
+        console.log(req);
+        console.log(req.body);
+
         db.isAuthenticated(req, res, function(flag, login_id){
             jsonData.auth_status=flag;
             if(flag){
